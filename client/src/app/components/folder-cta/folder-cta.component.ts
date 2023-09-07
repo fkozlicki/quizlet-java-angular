105b3b3aadb97b151f0cee043fa82d465517fdc5
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-09-07 23:21:15 +0200

    Create FolderCta component

---
+++ client/src/app/components/folder-cta/folder-cta.component.ts
@@ -0,0 +1,83 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { PicturePlaceholderComponent } from '../picture-placeholder/picture-placeholder.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ManageFolderModalComponent } from '../manage-folder-modal/manage-folder-modal.component';
+import { FolderModalComponent } from '../folder-modal/folder-modal.component';
+import { DeleteModalComponent } from '../delete-modal/delete-modal.component';
+
+@Component({
+  selector: 'app-folder-cta',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    PicturePlaceholderComponent,
+  ],
+  template: `
+    <div class="mb-4 flex flex-wrap items-center justify-between">
+      <div class="flex items-center gap-10">
+        <div class="font-medium text-gray-700">
+          {{ folder.studySets.length }} set{{
+            folder.studySets.length === 1 ? '' : 's'
+          }}
+        </div>
+        <div class="flex items-center gap-2">
+          <div class="font-medium text-gray-500">created by</div>
+          <app-picture-placeholder [size]="'small'"></app-picture-placeholder>
+          <div class="font-medium">
+            {{ folder.user.name }}
+          </div>
+        </div>
+      </div>
+      <div class="flex gap-2">
+        <button (click)="openDialog()" mat-mini-fab color="primary">
+          <mat-icon>add</mat-icon>
+        </button>
+        <button (click)="openEditFolder()" mat-mini-fab color="primary">
+          <mat-icon>edit</mat-icon>
+        </button>
+        <button (click)="openDeleteFolder()" mat-mini-fab color="primary">
+          <mat-icon>delete_outline</mat-icon>
+        </button>
+      </div>
+    </div>
+  `,
+})
+export class FolderCtaComponent {
+  @Input() folder!: Folder;
+  @Input() userId!: number;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(ManageFolderModalComponent, {
+      data: {
+        folderId: this.folder.id,
+        userId: this.userId,
+        folderSets: this.folder.studySets,
+      },
+    });
+  }
+
+  openEditFolder() {
+    this.dialog.open(FolderModalComponent, {
+      data: {
+        type: 'edit',
+        folder: this.folder,
+      },
+    });
+  }
+
+  openDeleteFolder() {
+    this.dialog.open(DeleteModalComponent, {
+      data: {
+        id: this.folder.id,
+        type: 'folder',
+      },
+    });
+  }
+}
