5cca913d97d2ed4f273304d224afca5b6112e602
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-09-07 23:58:26 +0200

    Added folders empty state

--- client/src/app/pages/folders/folders.component.ts
+++ client/src/app/pages/folders/folders.component.ts
@@ -4,6 +4,10 @@ import { UserLayoutComponent } from 'src/app/components/user-layout/user-layout.
 import { FolderService } from 'src/app/services/folder.service';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { SubscribeDirective } from '@ngneat/subscribe';
+import { MatDialog } from '@angular/material/dialog';
+import { FolderModalComponent } from 'src/app/components/folder-modal/folder-modal.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-folders',
@@ -13,21 +17,43 @@ import { SubscribeDirective } from '@ngneat/subscribe';
     UserLayoutComponent,
     SubscribeDirective,
     RouterModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
   ],
   template: `
     <app-user-layout>
-      <div *subscribe="foldersQuery$ as foldersQuery">
-        <div *ngIf="foldersQuery.data as folders" class="flex flex-col gap-4">
-          <a
-            *ngFor="let folder of folders"
-            [routerLink]="['/', userId, 'folders', folder.id]"
-            class="rounded bg-white p-3 shadow-md"
+      <div *subscribe="foldersQuery$ as foldersQuery" class="h-full">
+        <mat-spinner *ngIf="foldersQuery.isLoading"></mat-spinner>
+        <div *ngIf="foldersQuery.data as folders" class="h-full">
+          <div
+            *ngIf="folders.length === 0"
+            class="grid h-full place-items-center"
           >
-            <div class="font-medium">{{ folder.studySets.length }} sets</div>
-            <div class="text-xl font-bold">
-              {{ folder.name }}
+            <div class="text-center">
+              <div class="mb-4 text-3xl font-bold">
+                You have no folders yet.
+              </div>
+              <button
+                mat-flat-button
+                color="primary"
+                (click)="openCreateFolder()"
+              >
+                Create folder
+              </button>
             </div>
-          </a>
+          </div>
+          <div *ngIf="folders.length > 0" class="flex flex-col gap-4">
+            <a
+              *ngFor="let folder of folders"
+              [routerLink]="['/', userId, 'folders', folder.id]"
+              class="rounded bg-white p-3 shadow-md"
+            >
+              <div class="font-medium">{{ folder.studySets.length }} sets</div>
+              <div class="text-xl font-bold">
+                {{ folder.name }}
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </app-user-layout>
@@ -41,5 +67,14 @@ export class FoldersComponent {
   constructor(
     private folderService: FolderService,
     private route: ActivatedRoute,
+    private dialog: MatDialog,
   ) {}
+
+  openCreateFolder() {
+    this.dialog.open(FolderModalComponent, {
+      data: {
+        type: 'create',
+      },
+    });
+  }
 }
