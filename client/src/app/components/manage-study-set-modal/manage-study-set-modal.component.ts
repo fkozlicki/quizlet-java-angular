94ca6dd53217d9fffa9c1915cbb4438cf3c769d4
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-09-07 22:05:14 +0200

    Added create folder functionality to ManageStudySetModal

--- client/src/app/components/manage-study-set-modal/manage-study-set-modal.component.ts
+++ client/src/app/components/manage-study-set-modal/manage-study-set-modal.component.ts
@@ -2,24 +2,51 @@ import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FolderService } from 'src/app/services/folder.service';
 import { SessionService } from 'src/app/services/session.service';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { SubscribeDirective } from '@ngneat/subscribe';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FolderModalComponent } from '../folder-modal/folder-modal.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-manage-study-set-modal',
   standalone: true,
-  imports: [CommonModule, SubscribeDirective, MatIconModule],
+  imports: [
+    CommonModule,
+    SubscribeDirective,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+  ],
   template: `
     <div class="relative w-[750px] p-6">
-      <button (click)="close()" class="absolute right-4 top-4">
+      <button (click)="closeAddToFolder()" class="absolute right-4 top-4">
         <mat-icon>close</mat-icon>
       </button>
       <div class="mb-8 text-3xl font-bold">Add to folder</div>
       <div *subscribe="userFoldersQuery.result$ as folders">
-        <div *ngIf="folders.data">
+        <div *ngIf="folders.data as folders">
+          <div *ngIf="folders.length === 0" class="flex justify-center">
+            <div class="text-center">
+              <div class="mb-4 text-xl font-semibold">
+                You have no folders yet.
+              </div>
+              <button
+                mat-flat-button
+                color="primary"
+                (click)="openCreateFolder()"
+              >
+                Create folder
+              </button>
+            </div>
+          </div>
           <div
-            *ngFor="let folder of folders.data"
+            *ngFor="let folder of folders"
             (click)="
               isInFolder(folder.studySets)
                 ? removeSet(folder.id)
@@ -36,6 +63,7 @@ import { MatIconModule } from '@angular/material/icon';
             </div>
           </div>
         </div>
+        <mat-spinner *ngIf="folders.isLoading"></mat-spinner>
       </div>
     </div>
   `,
@@ -51,11 +79,21 @@ export class ManageStudySetModalComponent {
     private folderService: FolderService,
     private sessionService: SessionService,
     @Inject(MAT_DIALOG_DATA) private setId: number,
-    private dialogRef: MatDialogRef<ManageStudySetModalComponent>,
+    private addToFolderModal: MatDialogRef<ManageStudySetModalComponent>,
+    private dialog: MatDialog,
   ) {}
 
-  close(): void {
-    this.dialogRef.close();
+  openCreateFolder() {
+    this.closeAddToFolder();
+    this.dialog.open(FolderModalComponent, {
+      data: {
+        type: 'create',
+      },
+    });
+  }
+
+  closeAddToFolder(): void {
+    this.addToFolderModal.close();
   }
 
   isInFolder(studySets: StudySet[]): boolean {
