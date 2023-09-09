091bbcef0833b2b964c8b3b9b236d1109b3e467c
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-09-09 23:41:51 +0200

    Added ResourceNotFound and BadCredentials exception handlers

--- server/src/main/java/pl/fkozlicki/quizlet/exception/DefaultExceptionHandler.java
+++ server/src/main/java/pl/fkozlicki/quizlet/exception/DefaultExceptionHandler.java
@@ -3,6 +3,7 @@ package pl.fkozlicki.quizlet.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,6 +25,38 @@ public class DefaultExceptionHandler {
                         request.getRequestURI(),
                         exception.getMessage(),
                         HttpStatus.CONFLICT.value(),
+                        LocalDateTime.now()
+                ));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ApiError> handleResourceNotFoundExceptionException(
+            ResourceNotFoundException exception,
+            HttpServletRequest request
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ApiError(
+                        request.getRequestURI(),
+                        exception.getMessage(),
+                        HttpStatus.NOT_FOUND.value(),
+                        LocalDateTime.now()
+                ));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<ApiError> handleResourceNotFoundExceptionException(
+            BadCredentialsException exception,
+            HttpServletRequest request
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ApiError(
+                        request.getRequestURI(),
+                        exception.getMessage(),
+                        HttpStatus.UNAUTHORIZED.value(),
                         LocalDateTime.now()
                 ));
     }
